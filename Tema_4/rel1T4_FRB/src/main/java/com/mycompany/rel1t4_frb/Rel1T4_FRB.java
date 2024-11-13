6ffeafd35e92aa5812984ac7c3b92ef672a5760b
FranciscoRodBel <9442@cifpceuta.es>
2024-11-13 16:57:55 +0100

    nueva relación empezada

---
+++ Tema_4/rel1T4_FRB/src/main/java/com/mycompany/rel1t4_frb/Rel1T4_FRB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rel1t4_frb;
+
+/**
+ *
+ * @author Francisco
+ */
+public class Rel1T4_FRB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
