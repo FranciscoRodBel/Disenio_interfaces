adac99d733336f81ad36e570f3d76e31c6884ff8
FranciscoRodBel <9442@cifpceuta.es>
2024-11-17 20:55:52 +0100

    panel borrar terminado, checkbox no conseguido

--- Tema_4/rel1T4_FRB/src/main/java/com/mycompany/rel1t4_frb/PanelBorrar.java
+++ Tema_4/rel1T4_FRB/src/main/java/com/mycompany/rel1t4_frb/PanelBorrar.java
@@ -16,9 +16,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -49,6 +51,11 @@ public class PanelBorrar extends javax.swing.JPanel {
     JScrollPane scrollTabla = new JScrollPane(tablaMascotas);
     
     JLabel labelResultado = new JLabel("");
+    
+    JButton botonBorrar = new JButton("Borrar");
+    
+    ArrayList<String> idMascotas = new ArrayList<>();
+    
         
     /**
      * Creates new form PanelBorrar
@@ -107,10 +114,20 @@ public class PanelBorrar extends javax.swing.JPanel {
         tablaMascotas.getColumnModel().getColumn(3).setPreferredWidth(250);
         tablaMascotas.setDefaultEditor(Object.class, null);
         
+        botonBorrar.setPreferredSize(new Dimension(120, 40));
+        botonBorrar.setForeground(new Color(255,255,255));
+        botonBorrar.setBackground(new Color(241, 81, 82));
+        this.add(botonBorrar);
+        
+        layout.putConstraint(SpringLayout.WEST, botonBorrar, 180, SpringLayout.WEST, scrollTabla);
+        layout.putConstraint(SpringLayout.NORTH, botonBorrar, 170, SpringLayout.NORTH, scrollTabla);
+        botonBorrar.setVisible(false);
+        
         botonBuscar.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
 
+                idMascotas.clear();
                 modeloTabla.setRowCount(0);
                 labelResultado.setText("");
                 
@@ -126,6 +143,7 @@ public class PanelBorrar extends javax.swing.JPanel {
                     while (resultado.next()) {
                         
                         totalFilas++;
+                        idMascotas.add(resultado.getString("ID"));
                         
                         modeloTabla.addRow(new Object[]{
                             resultado.getString("nombre"),
@@ -139,15 +157,19 @@ public class PanelBorrar extends javax.swing.JPanel {
                     if (totalFilas == 0) {
                     
                         scrollTabla.setVisible(false);
+                        botonBorrar.setVisible(false);
                         labelResultado.setText("No se encontraron resultados");
                         
                     } else if(totalFilas == 1) {
                     
                         scrollTabla.setVisible(true);
+                        botonBorrar.setVisible(true);
                         
                     } else {
                         
                         scrollTabla.setVisible(true);
+                        botonBorrar.setVisible(true);
+                        
                         
                     }
 
@@ -157,9 +179,39 @@ public class PanelBorrar extends javax.swing.JPanel {
                 } 
             }   
         });
+       
+        botonBorrar.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                
+                scrollTabla.setVisible(false);
+                botonBorrar.setVisible(false);
+                inputBuscador.setText("");
+                      
+                for (int i = 0; i < idMascotas.size(); i++) {
+                    
+                    String consultaEliminar = "DELETE FROM mascota WHERE ID = '" + idMascotas.get(i) + "'";
+
+                    try {
+                        
+                        Statement consulta = conexion.createStatement();
+                        int filasEliminadas = consulta.executeUpdate(consultaEliminar);
+                        
+                    } catch (SQLException ex) {
+                        
+                        mostrarModal("Error al borrar la mascota");
+                            
+                    }
+                    
+                }
+
+                mostrarModal("Mascotas borradas correctamente");
+            }
+               
+        });
     }
     
-    public void mostrarModal() {
+    public void mostrarModal(String mensaje) {
     
         Window parentWindow = getWindowAncestor(this);
         JDialog modalMensaje = new JDialog((Frame) parentWindow, true);
@@ -169,7 +221,7 @@ public class PanelBorrar extends javax.swing.JPanel {
         modalMensaje.setSize(new Dimension(350, 150));
         modalMensaje.setLocationRelativeTo(PanelBorrar.this); // Para que aparezca el modal en el centro con respecto a al jframe principal
 
-        JLabel labelMensaje = new JLabel("Mascota borrada correctamente");
+        JLabel labelMensaje = new JLabel(mensaje);
         JButton botonAceptar = new JButton("Aceptar");
 
         labelResultado.setHorizontalAlignment(labelResultado.CENTER);
