69acd75eea2042910e3a29a25f838016aa5c32fb
FranciscoRodBel <9442@cifpceuta.es>
2025-01-11 16:21:07 +0100

    estructura de los modelos comenzada

---
+++ ProyectoFinal/TareApp/src/main/java/com/mycompany/tareapp/modelo/Tarea.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tareapp.modelo;
+
+/**
+ *
+ * @author Propietario
+ */
+public class Tarea {
+    
+    private int idTarea;
+    private int completada;
+    private String titulo;
+    private int prioridad;
+    private String fecha;
+    private String descripcion;
+    private int idLista;
+}
