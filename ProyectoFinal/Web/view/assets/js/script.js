9532c7627dc07913e1b8975248fcac60c1beea3e
FranciscoRodBel <9442@cifpceuta.es>
2025-05-04 15:51:46 +0200

    descargar Apk, añadido

--- ProyectoFinal/Web/view/assets/js/script.js
+++ ProyectoFinal/Web/view/assets/js/script.js
@@ -11,8 +11,8 @@ document.getElementById("boton_descargar_exe").addEventListener("click", functio
 document.getElementById("boton_descargar_apk").addEventListener("click", function() {
     
     const enlaceDescarga = document.createElement("a");
-    enlaceDescarga.href = "./view/assets/TareApp_installer.exe";
-    enlaceDescarga.download = "TareApp_installer.exe";
+    enlaceDescarga.href = "./view/assets/TareApp.apk";
+    enlaceDescarga.download = "TareApp.apk";
     document.body.appendChild(enlaceDescarga);
     enlaceDescarga.click();
     document.body.removeChild(enlaceDescarga);
